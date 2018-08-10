10afe6fd0458f898712f6080b6ea67d907a704e4
Daniel Meier <daniel.meier@adesso.de>
2018-08-10 11:08:19 +0200

    initial backend commit

---
+++ backend/project-board/src/test/java/de/adesso/projectboard/ProjectBoardApplicationTests.java
@@ -0,0 +1,16 @@
+package de.adesso.projectboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectBoardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
