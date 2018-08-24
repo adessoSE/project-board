a1545bd3adc3faac95695aff955b6be70282c599
Daniel Meier <daniel.meier@adesso.de>
2018-08-24 13:14:30 +0200

    + modified template message

--- backend/project-board/src/main/java/de/adesso/projectboard/core/mail/ApplicationTemplateMessage.java
+++ backend/project-board/src/main/java/de/adesso/projectboard/core/mail/ApplicationTemplateMessage.java
@@ -8,9 +8,19 @@ import org.springframework.mail.SimpleMailMessage;
  */
 public class ApplicationTemplateMessage extends SimpleMailMessage {
 
-    private final String DISCLAIMER = "This is a automatically generated message, please do not reply!";
+    private static final String DISCLAIMER = "\n\n\nThis is a automatically generated message, please do not reply!";
 
-    private final String TITLE_PATTERN = "New application for JIRA Project %s";
+    private static final String TITLE_PATTERN = "New application for JIRA Project %s";
+
+    private static final String MAIN_TEXT_PATTERN = "%s applied for the JIRA Project \"%s\".";
+
+    private static final String COMMENT_PATTERN = "\n\nAdditional comment from user:\n\n\"%s\"\n\n";
+
+    private final String applicantName;
+
+    private final String comment;
+
+    private final JiraProject project;
 
     /**
      * Constructs a new template message.
@@ -19,15 +29,28 @@ public class ApplicationTemplateMessage extends SimpleMailMessage {
      *          The {@link JiraProject} the application message refers to.
      *
      * @param comment
-     *          A additional comment.
+     *          A additional comment message.
+     *
+     * @param applicantName
+     *          The name of the applicant.
      */
-    public ApplicationTemplateMessage(JiraProject project, String comment) {
+    public ApplicationTemplateMessage(JiraProject project, String comment, String applicantName) {
+        this.project = project;
+        this.comment = comment;
+        this.applicantName = applicantName;
+
         setSubject(String.format(TITLE_PATTERN, project.getKey()));
         setText(buildText());
     }
 
     private String buildText() {
         StringBuilder builder = new StringBuilder();
+
+        builder.append(String.format(MAIN_TEXT_PATTERN, applicantName, project.getTitle()));
+
+        if(comment != null && !comment.isEmpty()) {
+            builder.append(String.format(COMMENT_PATTERN, comment));
+        }
 
         builder.append(DISCLAIMER);
 
