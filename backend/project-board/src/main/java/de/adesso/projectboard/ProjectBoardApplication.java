7d491d3917dee6916b9018011551db6eca3e358d
Daniel Meier <daniel.meier@adesso.de>
2018-08-28 13:24:27 +0200

    + restructred dependencies + removed redundant annotations

--- backend/project-board/src/main/java/de/adesso/projectboard/ProjectBoardApplication.java
+++ backend/project-board/src/main/java/de/adesso/projectboard/ProjectBoardApplication.java
@@ -2,16 +2,12 @@ package de.adesso.projectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
-@EntityScan
-@EnableJpaRepositories
 public class ProjectBoardApplication {
 
 	public static void main(String[] args) {
