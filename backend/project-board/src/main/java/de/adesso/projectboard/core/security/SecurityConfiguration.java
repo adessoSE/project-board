83c0b46e2a5217f2c8605cfe2611e16a8ac28337
Daniel Meier <daniel.meier@adesso.de>
2018-09-04 13:34:38 +0200

    + disabled cors

--- backend/project-board/src/main/java/de/adesso/projectboard/core/security/SecurityConfiguration.java
+++ backend/project-board/src/main/java/de/adesso/projectboard/core/security/SecurityConfiguration.java
@@ -45,7 +45,7 @@ public class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter 
 
         http
                 .cors()
-                .and()
+                    .disable()
                 .csrf()
                     .disable()
                 .sessionManagement()
