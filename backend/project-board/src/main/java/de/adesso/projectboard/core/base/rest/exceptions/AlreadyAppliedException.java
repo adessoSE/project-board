855a1f67878bb8390a12f9a74c23996f14fcfe19
Daniel Meier <daniel.meier@adesso.de>
2018-09-26 16:08:33 +0200

    + modified exception status code

--- backend/project-board/src/main/java/de/adesso/projectboard/core/base/rest/exceptions/AlreadyAppliedException.java
+++ backend/project-board/src/main/java/de/adesso/projectboard/core/base/rest/exceptions/AlreadyAppliedException.java
@@ -3,7 +3,7 @@ package de.adesso.projectboard.core.base.rest.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Already applied for this project!")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Already applied for this project!")
 public class AlreadyAppliedException extends RuntimeException {
 
 }
