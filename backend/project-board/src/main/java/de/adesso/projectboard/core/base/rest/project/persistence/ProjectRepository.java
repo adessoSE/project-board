fb86dbfc634e2a0a1530af2ee9e565aed3ac3e46
Daniel Meier <daniel.meier@adesso.de>
2018-10-02 16:59:33 +0200

    - removed unused method + added @Param annotation to query method

--- backend/project-board/src/main/java/de/adesso/projectboard/core/base/rest/project/persistence/ProjectRepository.java
+++ backend/project-board/src/main/java/de/adesso/projectboard/core/base/rest/project/persistence/ProjectRepository.java
@@ -2,6 +2,7 @@ package de.adesso.projectboard.core.base.rest.project.persistence;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,8 +10,6 @@ import java.util.List;
  * {@link CrudRepository} for persisting {@link Project}s.
  */
 public interface ProjectRepository extends CrudRepository<Project, String> {
-
-    List<Project> findAllByStatusOrStatusIgnoreCaseOrderByCreatedDesc(String status, String otherStatus);
 
     // a normal user can see escalated projects and open ones when the user's
     // LOB is the same as the project's one
@@ -20,7 +19,7 @@ public interface ProjectRepository extends CrudRepository<Project, String> {
             "WHERE (lower(p.status) = lower('eskaliert')) " +
             "OR (lower(p.status) = lower('offen') AND (lower(p.lob) = lower(:lob) OR p.lob IS NULL))" +
             "ORDER BY p.updated DESC")
-    List<Project> getAllForUserOfLob(String lob);
+    List<Project> getAllForUserOfLob(@Param("lob") String lob);
 
     // super users can see all escalated and open projects
     @Query("SELECT p " +
