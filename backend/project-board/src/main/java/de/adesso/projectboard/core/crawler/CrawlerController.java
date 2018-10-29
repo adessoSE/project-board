5a0a61c5a1667613a7cbd518697b641b7b90a01f
Daniel Meier <daniel.meier@adesso.de>
2018-10-29 18:44:07 +0100

    + added tokens in property files and their corresponding defaults + added Profile annotation on CrawlerController

--- backend/project-board/src/main/java/de/adesso/projectboard/core/crawler/CrawlerController.java
+++ backend/project-board/src/main/java/de/adesso/projectboard/core/crawler/CrawlerController.java
@@ -1,11 +1,13 @@
 package de.adesso.projectboard.core.crawler;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("adesso-ad")
 @RestController
 @RequestMapping("/crawler")
 public class CrawlerController {
