51795b02d4eeea91c4c8f56c409c78617380540f
Alexander Brockmann <brock021@freenet.de>
2018-12-14 11:56:14 +0100

    added corsmapping

--- backend/project-board/src/main/java/de/adesso/projectboard/base/configuration/RestConfiguration.java
+++ backend/project-board/src/main/java/de/adesso/projectboard/base/configuration/RestConfiguration.java
@@ -6,5 +6,14 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 @Configuration
 @EnableSpringDataWebSupport
 public class RestConfiguration {
-    
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurerAdapter(){
+            @Override
+            private void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/").allowedOrigins("*");
+            }
+        }
+    }
 }
+
