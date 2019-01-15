b328a2d977bccce125514133543287200ff57eef
pschuerk <peter.schuerk@adesso.de>
2019-01-15 13:46:29 +0100

    + added a cleanup method for the project text-fields + linebreaks are now shown in the project dialog + rows that have no data to show (null values) are not longer displayed

--- frontend/project-board-app/src/app/project-dialog/project-dialog.component.ts
+++ frontend/project-board-app/src/app/project-dialog/project-dialog.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { NavigationStart, Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { AuthenticationService } from '../_services/authentication.service';
 import { Application, EmployeeService } from '../_services/employee.service';
 import { Project } from '../_services/project.service';
-import { Router, NavigationStart } from '@angular/router';
 
 export interface ProjectDialogData {
   project: Project;
@@ -33,22 +33,42 @@ export class ProjectDialogComponent implements OnInit {
   showBox = false;
   comment = '';
 
-  ngOnInit() {
-    this.mobile = document.body.clientWidth < 992;
-  }
-
   constructor(
     public dialogRef: MatDialogRef<ProjectDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: ProjectDialogData,
     private authService: AuthenticationService,
     private employeeService: EmployeeService,
-    private router: Router,) 
-    { router.events.subscribe( event => {
-        if(event instanceof NavigationStart) {
-          this.dialogRef.close();
-        }
-      });
+    private router: Router) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.dialogRef.close();
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.mobile = document.body.clientWidth < 992;
+    this.data.project.skills = this.cleanUpString(this.data.project.skills);
+    this.data.project.description = this.cleanUpString(this.data.project.description);
+    this.data.project.job = this.cleanUpString(this.data.project.job);
+    this.data.project.title = this.cleanUpString(this.data.project.title);
+  }
+
+  cleanUpString(string: string): string {
+    if (string) {
+      string = string.replace(/\n{2,}/gm, '\n');
+      string = string.replace(/\t{2,}/gm, '\t');
+      string = string.replace(/ {2,}/gm, ' ');
+      string = string.replace(/\A\s*/gm, '');
+      string = string.replace(/\s*\z/gm, '');
+      string = string.replace(/^\s$/gm, '');
+      string = string.replace(/\s*,/, ',');
+      string = string.replace(/,\w/, ', ');
+      string = string.replace(/,[^ ]/, '');
+      return string;
     }
+    return null;
+  }
 
   toggleRequestArea() {
     this.showBox = !this.showBox;
