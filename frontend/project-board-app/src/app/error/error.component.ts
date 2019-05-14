973d91c2b6229a5031e46251002d86c0a3d7f57d
Daniel Meier <daniel.meier@adesso.de>
2019-05-14 18:05:01 +0200

    + add missing supportEmail property in error component

--- frontend/project-board-app/src/app/error/error.component.ts
+++ frontend/project-board-app/src/app/error/error.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-error',
@@ -7,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
   message: string;
+  supportEmail: string;
 
   constructor() { }
 
   ngOnInit(): void {
     this.message = 'Da ist etwas schief gelaufen. Sollte der Fehler nicht zu beheben sein, kontaktiere den project board Support.';
+    this.supportEmail = environment.supportEmail;
   }
 }
